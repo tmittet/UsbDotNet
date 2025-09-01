2ce9697aaa2744ce0a14b2b67b67d6dba8844917
Thomas Mittet <thomas@huddly.com>
2025-09-01 16:35:22 +0200

    Add support for UVC requests

---
+++ tests/LibUsbSharp.Extensions.Tests/ControlTransfer/Uvc/Given_a_video_class_USB_device_with_UVC.cs
@@ -0,0 +1,122 @@
+﻿using LibUsbSharp.Extensions.ControlTransfer;
+using LibUsbSharp.Extensions.ControlTransfer.Uvc;
+
+namespace LibUsbSharp.Extensions.Tests.ControlTransfer.Uvc;
+
+[Trait("Category", "UsbVideoControl")]
+public sealed class Given_a_video_class_USB_device_with_UVC : IDisposable
+{
+    private const byte UvcInterfaceSubClass = 0x01; // SC_VIDEOCONTROL
+    private const byte ControlSelector = 0x02;
+    private const byte ProcessingUnit = 0x03;
+
+    private readonly ILoggerFactory _loggerFactory;
+    private readonly ILogger<Given_a_video_class_USB_device_with_UVC> _logger;
+    private readonly LibUsb _libUsb;
+    private readonly TestDeviceSource _deviceSource;
+
+    public Given_a_video_class_USB_device_with_UVC(ITestOutputHelper output)
+    {
+        _loggerFactory = new TestLoggerFactory(output);
+        _logger = _loggerFactory.CreateLogger<Given_a_video_class_USB_device_with_UVC>();
+        _libUsb = new LibUsb(_loggerFactory);
+        _libUsb.Initialize(LogLevel.Information);
+        _deviceSource = new TestDeviceSource(_logger, _libUsb);
+        _deviceSource.SetPreferredVendorId(0x2BD9);
+        _deviceSource.SetRequiredInterfaceClass(UsbClass.Video, TestDeviceAccess.Control);
+        _deviceSource.SetRequiredInterfaceSubClass(UvcInterfaceSubClass);
+    }
+
+    [SkippableFact]
+    public void ControUvcRead_Brightness_should_complete_successfully()
+    {
+        using var device = _deviceSource.OpenUsbDeviceOrSkip();
+        var serial = device.GetSerialNumber();
+        var uvcInterfaces = device.GetInterfaceDescriptors(UsbClass.Video, UvcInterfaceSubClass);
+        var uvcInterface = uvcInterfaces.First();
+        _logger.LogInformation(
+            "Video device open: VID=0x{VID:X4}, PID=0x{PID:X4}, "
+                + "SerialNumber={SerialNumber}, UVC interface: {Interface}.",
+            device.Descriptor.VendorId,
+            device.Descriptor.ProductId,
+            serial,
+            uvcInterface.InterfaceNumber
+        );
+        var readBuffer = new Span<byte>(new byte[2]);
+        var result = device.ControlRead(
+            readBuffer,
+            out var readLength,
+            ControlRequestUvc.GetCurrent,
+            uvcInterface.InterfaceNumber,
+            ProcessingUnit,
+            ControlSelector,
+            timeout: 500
+        );
+        var readValue = BitConverter.ToInt16(readBuffer);
+        result.Should().Be(LibUsbResult.Success);
+    }
+
+    [SkippableFact]
+    public void ControUvcWrite_Brightness_should_complete_successfully()
+    {
+        using var device = _deviceSource.OpenUsbDeviceOrSkip();
+        var serial = device.GetSerialNumber();
+        var uvcInterface = device.GetInterfaceDescriptors(UsbClass.Video, UvcInterfaceSubClass).First();
+        _logger.LogInformation(
+            "Video device open: VID=0x{VID:X4}, PID=0x{PID:X4}, "
+                + "SerialNumber={SerialNumber}, UVC interface: {Interface}.",
+            device.Descriptor.VendorId,
+            device.Descriptor.ProductId,
+            serial,
+            uvcInterface.InterfaceNumber
+        );
+        var initialValueBuffer = new Span<byte>(new byte[2]);
+        var initialReadResult = device.ControlRead(
+            initialValueBuffer,
+            out var initialBytesRead,
+            ControlRequestUvc.GetCurrent,
+            uvcInterface.InterfaceNumber,
+            ProcessingUnit,
+            ControlSelector,
+            timeout: 500
+        );
+        if (initialReadResult != LibUsbResult.Success)
+        {
+            throw new SkipException($"ControlRead result '{initialReadResult}', {initialBytesRead} bytes read.");
+        }
+
+        var initialValue = BitConverter.ToInt16(initialValueBuffer);
+        var newValue = (short)(initialValue > 400 ? -600 : initialValue + 150);
+        var writeBuffer = new Span<byte>(BitConverter.GetBytes(newValue));
+        var writeResult = device.ControlWrite(
+            writeBuffer,
+            out var bytesWritten,
+            ControlRequestUvc.SetCurrent,
+            uvcInterface.InterfaceNumber,
+            ProcessingUnit,
+            ControlSelector,
+            timeout: 500
+        );
+        writeResult.Should().Be(LibUsbResult.Success);
+        bytesWritten.Should().Be(2);
+
+        var newValueBuffer = new Span<byte>(new byte[2]);
+        var newReadResult = device.ControlRead(
+            newValueBuffer,
+            out var newBytesRead,
+            ControlRequestUvc.GetCurrent,
+            uvcInterface.InterfaceNumber,
+            ProcessingUnit,
+            ControlSelector,
+            timeout: 500
+        );
+        newReadResult.Should().Be(LibUsbResult.Success);
+        newBytesRead.Should().Be(2);
+        BitConverter.ToInt16(newValueBuffer).Should().Be(newValue);
+    }
+
+    public void Dispose()
+    {
+        _libUsb.Dispose();
+    }
+}
